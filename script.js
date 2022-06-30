765253c1c8901f009df4d4807c04b5661073f8e1
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-07-01 00:28:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const settingsBtn = document.querySelector('#setting-btn');
+const settings = document.querySelector('#settings');
+const difficultySelect = document.querySelector('#difficulty');
+const word = document.querySelector('#word');
+const inputText = document.querySelector('#input-text');
+const scoreEl = document.querySelector('#score');
+const timeEl = document.querySelector('#time');
+const endGameEl = document.querySelector('#end-game-container');
